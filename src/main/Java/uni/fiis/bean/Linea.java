aed0799605363e00572746e137e2b484115afc2f
dahsser <deidago7@hotmail.com>
2015-06-20 17:36:04 -0500

    Mejorando pagina

---
+++ src/main/Java/uni/fiis/bean/Linea.java
@@ -0,0 +1,27 @@
+package uni.fiis.bean;
+
+import java.util.List;
+
+/**
+ * Created by Diego on 20/06/2015.
+ */
+public class Linea {
+    private List<Ubicacion> estaciones;
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Ubicacion> getEstaciones() {
+        return estaciones;
+    }
+
+    public void setEstaciones(List<Ubicacion> estaciones) {
+        this.estaciones = estaciones;
+    }
+}
